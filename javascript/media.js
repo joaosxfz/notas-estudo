0d17e9d480d3826b2c3b5ed727d71fce0f497afc
joaov <jvictorcs35@gmail.com>
2025-11-10 19:37:34 -0300

    kill bill

--- javascript/media.js
+++ javascript/media.js
@@ -1,2 +1,7 @@
 //FUP que calcula a média de 3 números
-const num1 = 
+const num1 = 4
+const num2 = 9
+const num3 = 2
+const resultado = (num1+num2+num3) / 3
+
+console.log("a média dos 3 números é " +resultado)
